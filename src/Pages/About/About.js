20f09af53a0c751ce8fcb9c92998f73828347c67
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-12-11 15:11:27 +0600

    about section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,6 +4,15 @@ const About = () => {
     return (
         <div>
             this is about page
+
+            <div>
+            Creative IT Institute is a trusted organization dedicated to creating breakthroughs in IT. Since its inception, Creative IT has achieved multifaceted success in the long 13 years. Contributing to the construction of digital Bangladesh. One of the leading IT companies in Bangladesh has been playing an important role in eliminating the unemployment problem since 2008.
+            </div>
+
+            <div>
+            purpose
+Empowering the people of the country by harnessing the power of youth and ensuring modern and quality training methods.
+            </div>
         </div>
     );
 };
