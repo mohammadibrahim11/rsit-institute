ed3e7868747a77d8671720d3fa52b995ac07898f
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-12-12 14:45:13 +0600

    create about section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,19 +1,125 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
     return (
         <div>
-            this is about page
-
+              
             <div>
-            Creative IT Institute is a trusted organization dedicated to creating breakthroughs in IT. Since its inception, Creative IT has achieved multifaceted success in the long 13 years. Contributing to the construction of digital Bangladesh. One of the leading IT companies in Bangladesh has been playing an important role in eliminating the unemployment problem since 2008.
+                <p className='fs-3 fw-bold '>About Us</p>
             </div>
 
-            <div>
-            purpose
-Empowering the people of the country by harnessing the power of youth and ensuring modern and quality training methods.
+           <div className='container d-flex jusfify-content-center align-items-center'>
+           <div className='border border-0 shadow col-6 h-50'  >
+           <p className='fs-6 fw-semibold  '>
+           RS-IT is Link trusted organization dedicated to creating breakthroughs in IT. Since its inception, Creative IT has achieved multifaceted success in the long 13 years. Contributing to the construction of digital Bangladesh. One of the leading IT companies in Bangladesh has been playing an important role in eliminating the unemployment problem since 2008.
+           </p>
             </div>
-        </div>
+
+            <div className='border border-0 shadow col-6 h-50'>
+              <h4>Purpose</h4>
+            
+             <p> Empowering the people of the country by harnessing the power of youth and ensuring modern and quality training methods.</p>
+            </div>
+           </div>
+
+
+           <div className='mt-5 mb-5 pt-5 pb-5'>
+            <p className='fs-3 fw-bold text-primary mb-4'>
+Office and Training Space </p>
+           {/* <!-- Gallery -->
+           <Link href="https://ibb.co/7Q1776k"><img src="https://i.ibb.co/DYR33Sr/311458593-1330280050843262-8164358592460139545-n.jpg" 
+           alt="311458593-1330280050843262-8164358592460139545-n" border="0"></Link >
+
+<Link href="https://ibb.co/F6QNvbh"><img src="https://i.ibb.co/kKLkngG/311412731-1330279990843268-6492179999814155791-n.jpg" 
+alt="311412731-1330279990843268-6492179999814155791-n" border="0"></Link >
+
+
+<Link href="https://ibb.co/hd3jfkS"><img src="https://i.ibb.co/n1xvCTF/311457018-1330279934176607-2170225972687093846-n.jpg" alt="311457018-1330279934176607-2170225972687093846-n" border="0"></Link >
+
+<Link href="https://ibb.co/rsLR1yS"><img src="https://i.ibb.co/9tfkCYL/311341426-1330279807509953-8969085145398983470-n.jpg" alt="311341426-1330279807509953-8969085145398983470-n" border="0"></Link >
+
+
+<Link href="https://ibb.co/C2cmRLX"><img src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt="316555174-1361487154389218-32141481641435038-n" border="0"></Link >
+
+
+<Link href="https://ibb.co/fpgXkSm"><img src="https://i.ibb.co/yy9N6RL/rsz-316664887-1361486957722571-6078300785716309152-n.jpg" alt="rsz-316664887-1361486957722571-6078300785716309152-n" border="0"></Link > */}
+
+<div class="container">
+
+  {/* <h1 class="fw-light text-center text-lg-start mt-4 mb-0">Thumbnail Gallery</h1> */}
+
+  <hr class="mt-2 mb-5"/>
+
+  <div class="row text-center text-lg-start">
+
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/9tfkCYL/311341426-1330279807509953-8969085145398983470-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <Link href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="https://i.ibb.co/8cCN1kR/316555174-1361487154389218-32141481641435038-n.jpg" alt=""/>
+      </Link >
+    </div>
+  </div>
+
+</div>
+
+
+</div>
+</div>
     );
 };
 
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg  container">
+    <div className="">
+      <nav className="navbar navbar-expand-lg ps-5 pe-5 ">
         <div className="container-fluid">
           <button
             className="navbar-toggler"
@@ -17,35 +18,35 @@ const Header = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <Link className="navbar-brand text-primary fw-bold hover:text-danger" to="">
+          <Link className="navbar-brand text-white fw-bold hover:text-danger" to="">
             RS IT
           </Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <ul className="navbar-nav m-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/home">
+            <ul className="navbar-nav m-auto mb-2 mb-lg-0 fw-bold ">
+              <li className="nav-item ">
+                <Link className="nav-link  text-white pe-4" aria-current="page" to="/home">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <Link className="nav-link text-white pe-4"  to="/about">
                   about
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link " to="/courses">
+                <Link className="nav-link text-white pe-4" to="/courses">
                  Courses
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link " to="/contactus">
+                <Link className="nav-link text-white  pe-4 " to="/contactus">
                  Contact us
                 </Link>
               </li>
             </ul>
        
              
-              <button className="btn btn-outline-danger" type="submit">
+              <button className="btn btn-outline-primary border-0 text-white" type="submit">
               Login
               </button>
            
